f49be2733365742ab274e07100cafc8a77c45cf8
topanb11 <topanbudiman11@gmail.com>
2022-05-28 19:39:44 -0600

    Finished homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,14 +20,14 @@ function Navbar() {
 				<h1 className='text-4xl font-Raleway font-bold pl-6 py-5 text-left bg-primary'>
 					Topan Budiman
 				</h1>
-				<div className='absolute right-4 top-6 cursor-pointer md:hidden'
+				<div className='absolute right-4 top-5 cursor-pointer md:hidden'
 					onClick={() => setOpen(!open)}
 				>
-					{open ? <CloseIcon fontSize='large' /> : <MenuIcon fontSize='large' />}
+					{open ? <CloseIcon sx={{fontSize: '40px' }} /> : <MenuIcon sx={{fontSize: '40px' }} />}
 				</div>
-				<ul className={` absolute pl-7 bg-secondary to-dark w-full z-[-1]
-				md:flex md:z-auto md:my-5 md:static md:flex md:items-center md:w-auto md:bg-primary md:transition-none
-				 transition-all duration-500 ease-in rounded-b-xl ${open ? 'top-20' : 'top-[-300px]'}`}>
+				<ul className={`absolute pl-7 bg-secondary w-full z-[-1]
+				md:flex md:z-auto md:static md:flex md:items-center md:w-auto md:bg-primary
+				transition-all duration-500 ease-in rounded-b-xl ${open ? 'top-16' : 'top-[-300px]'}`}>
 					{
 						Links.map((link) => (
 							<li key={link.name} className='pr-6 my-7 md:px-10 text-2xl font-semibold duration-500
