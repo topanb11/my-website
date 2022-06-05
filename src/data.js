70c88002b1a952aea8ff7a0d7594be4ab2e6172c
topanb11 <topanbudiman11@gmail.com>
2022-06-05 01:14:22 -0600

    Added data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+
+const data = {
+	ExperienceData: [
+		{
+			company: 'Ovintiv',
+			imageURL: 'https://pbs.twimg.com/profile_images/1221057214368165889/xzvuWDwn_400x400.png',
+			order: 1,
+			position: [
+				{
+					title: 'Land Innovation Intern',
+					duration: 'May. 2022 - Aug. 2022',
+					location: 'Calgary, AB'
+				}
+			]
+		},
+		{
+			company: 'Code the Change YYC',
+			imageURL: 'https://codethechangeyyc.ca/images/_logo.png',
+			order: 2,
+			position: [
+				{
+					title: 'Co-President',
+					duration: 'Mar. 2022 - Present',
+					location: 'Calgary, AB'
+				},
+				{
+					title: 'Project Team Lead',
+					duration: 'Mar. 2022 - Present',
+					location: 'Calgary, AB'
+				},
+			]
+		}
+	]
+}
+export default data;
